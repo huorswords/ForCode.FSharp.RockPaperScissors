2d1696f718964bc5121b72d0422a9c5e873a9c5c
Ángel García Santos <angel.g-santos@outlook.com>
2015-11-18 06:53:17 +0100

    First version of the Kata

---
+++ ForCode.RockPaperScissors.UnitTests/RockPaperScissorsTests.fs
@@ -0,0 +1,46 @@
+﻿namespace ForCode.RockPaperScissors.UnitTests
+
+open System
+open Xunit
+open Xunit.Extensions
+
+module RockPaperScissorsTests = 
+    open ForCode.RockPaperScissors.RockPaperScissorsResolver
+        
+    [<Theory>]
+    [<InlineData(Hand.Scissors, Hand.Paper, Hand.Scissors)>]
+    [<InlineData(Hand.Scissors, Hand.Rock, Hand.Rock)>]
+    [<InlineData(Hand.Scissors, Hand.Scissors, Hand.None)>]
+    [<InlineData(Hand.Paper, Hand.Scissors, Hand.Scissors)>]
+    [<InlineData(Hand.Rock, Hand.Scissors, Hand.Rock)>]
+    [<InlineData(Hand.Paper, Hand.Rock, Hand.Paper)>]
+    [<InlineData(Hand.Scissors, Hand.Rock, Hand.Rock)>]
+    [<InlineData(Hand.Rock, Hand.Paper, Hand.Paper)>]
+    [<InlineData(Hand.Rock, Hand.Scissors, Hand.Rock)>]
+    [<InlineData(Hand.Rock, Hand.Rock, Hand.None)>]
+    [<InlineData(Hand.Paper, Hand.Scissors, Hand.Scissors)>]
+    [<InlineData(Hand.Paper, Hand.Rock, Hand.Paper)>]
+    [<InlineData(Hand.Scissors, Hand.Paper, Hand.Scissors)>]
+    [<InlineData(Hand.Rock, Hand.Paper, Hand.Paper)>]
+    [<InlineData(Hand.Paper, Hand.Paper, Hand.None)>]    
+    let WinnerIsCorrect
+        (left : Hand)
+        (right : Hand) 
+        (expected : Hand) =
+        let actual = GetWinner left right
+        let expected = expected
+        Assert.Equal(expected, actual)
+    
+    [<Theory>]
+    [<InlineData(Hand.None, Hand.Scissors)>]
+    [<InlineData(Hand.None, Hand.None)>]
+    [<InlineData(Hand.Scissors, Hand.None)>]
+    let AnyInvalidHandRaisesException
+        (left : Hand)
+        (right : Hand) =
+
+        try
+            let actual = GetWinner left right
+            Assert.True(false)
+        with
+         | Failure(msg) -> printfn "%s" msg;
---
+++ ForCode.RockPaperScissors/RockPaperScissors.fs
@@ -0,0 +1,44 @@
+﻿namespace ForCode.RockPaperScissors
+
+module RockPaperScissorsResolver =
+    type Hand =
+        | None = 0
+        | Scissors = 1
+        | Rock = 2
+        | Paper = 3
+    
+    let GetWinner 
+        (left : Hand)
+        (right : Hand) =
+
+        if left.Equals(Hand.None)
+            || right.Equals(Hand.None) then
+            failwith "Invalid hand"
+        
+        match left with
+        | Hand.Scissors ->
+            match right with
+            | Hand.Paper ->
+                left
+            | Hand.Rock ->
+                right
+            | _ ->           
+                Hand.None
+        | Hand.Paper ->
+            match right with
+            | Hand.Rock ->
+                left
+            | Hand.Scissors ->
+                right
+            | _ ->           
+                Hand.None
+        | Hand.Rock ->
+            match right with
+            | Hand.Scissors ->
+                left
+            | Hand.Paper ->
+                right
+            | _ ->           
+                Hand.None
+        | _ ->           
+            failwith "Invalid hand"
